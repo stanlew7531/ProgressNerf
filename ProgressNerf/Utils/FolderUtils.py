28018ee0bae3a1d4b36bb21c24ccbbc65df3d850
Stanley Lewis <stanlew@umich.edu>
2022-01-17 11:34:00 -0500

    load from epoch & resume training changes

---
+++ ProgressNerf/Utils/FolderUtils.py
@@ -0,0 +1,7 @@
+import os
+import glob
+
+def last_epoch_from_output_dir(directory:str):
+    folders = glob.glob(os.path.join(directory, "epoch_*"))
+    epochs = [int(folder.replace(os.path.join(directory, "epoch_"),'')) for folder in folders]
+    return max(epochs)
